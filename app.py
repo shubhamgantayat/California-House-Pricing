6b86467d9305ff11102f45ac6ac308b227fafeaa
Shubham Gantayat <sgantayat9@gmail.com>
2021-08-18 17:38:59 +0530

    try-catch added

--- app.py
+++ app.py
@@ -16,18 +16,21 @@ def home_page():
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        california_housing_model = joblib.load("california_housing.pkl")
-        columns = ['longitude', 'latitude', 'housing_median_age', 'total_rooms',
-                   'total_bedrooms', 'population', 'households', 'median_income',
-                   'ocean_proximity']
-        values = dict()
-        for col in columns:
-            if col == 'ocean_proximity':
-                values[col] = [request.form[col]]
-            else:
-                values[col] = [float(request.form[col])]
-        df = pd.DataFrame.from_dict(values)
-        result = california_housing_model.predict(df)[0]
+        try:
+            california_housing_model = joblib.load("california_housing.pkl")
+            columns = ['longitude', 'latitude', 'housing_median_age', 'total_rooms',
+                       'total_bedrooms', 'population', 'households', 'median_income',
+                       'ocean_proximity']
+            values = dict()
+            for col in columns:
+                if col == 'ocean_proximity':
+                    values[col] = [request.form[col]]
+                else:
+                    values[col] = [float(request.form[col])]
+            df = pd.DataFrame.from_dict(values)
+            result = california_housing_model.predict(df)[0]
+        except:
+            result = "Please enter valid inputs"
         return render_template("form.html", results=result)
 
 
